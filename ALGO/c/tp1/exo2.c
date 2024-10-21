0e7e0b9ff41938ce3a08e23dc667272a1df67301
Victorien Denoyelle <victorien.denoyelle@epitech.eu>
2024-10-21 17:12:48 +0200

    add update on algo's tp

--- ALGO/c/tp1/exo2.c
+++ ALGO/c/tp1/exo2.c
@@ -2,9 +2,10 @@
 
 int main() {
     float ht;
+
     printf("Donne le rpix ht batard\n");
     scanf("%f", &ht);
     ht *= 1.20;
-    printf("Le prix TTC est pour cet article est de : %f", ht);
+    printf("Le prix TTC est pour cet article est de : %.2f\n", ht);
     return 0;
 }
